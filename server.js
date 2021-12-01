f2bc4456347e1d042656ae140c202bcbb62b2f46
IshtiaqueNafis <nafu22@gmail.com>
2021-12-01 08:59:32 -0500

    created router file employees

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//route files
+const employees = require('./routes/employees')
+
 //load config
 
 dotenv.config({path: './config/config.env'});
 
+
+
 const app = express();
+
+//mount routes
+
+app.use('/employees', employees )
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
